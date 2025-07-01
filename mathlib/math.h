5f49dad13a60a0932fe5870fb490459640fcb861
Simon <zsework1@gmail.com>
2025-07-01 13:26:11 +0000

    mathlib deleted

--- mathlib/math.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace math {
-double addition(double firstValue, double secondValue);
-double multiple(double firstValue, double secondValue);
-double divided(double firstValue, double secondValue, bool &zeroCheck);
-double power(double firstValue, double secondValue);
-int findFactorial(int value);
-}
